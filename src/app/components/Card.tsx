eaecf8c0bc8afb52b4ef761e71dd4cd555bf4b4d
Quach Trong Nhan <lhv@Quachs-MacBook-Pro.local>
2023-12-26 09:26:13 +0700

    fix scrolling on mobile

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -32,7 +32,7 @@ const Card: React.FC<ICardProps> = ({
 }) => {
   return (
     <div
-      className={`mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 z-10 absolute top-0 left-0 right-0 max-h-screen overflow-hidden rounded-2xl`}
+      className={`mx-auto max-w-7xl py-24 sm:px-6 sm:py-32 lg:px-8 z-10 absolute top-0 left-0 right-0 max-h-screen overflow-auto rounded-2xl`}
     >
       <div className="relative isolate overflow-hidden px-6 pt-16 shadow-2xl sm:rounded-3xl sm:px-16 md:pt-24 lg:flex lg:gap-x-20 lg:px-24 lg:pt-0 bg-[radial-gradient(ellipse_at_top_right,_var(--tw-gradient-stops))] from-pink-300 via-purple-300 to-indigo-400">
         <Form
